7655fc245f720bcc3bf5c08164a244fe5ac03155
Martin Gausby <martin.gausby@erlang-solutions.com>
2020-11-03 14:24:36 +0000

    Clean up: Remove the old app handler behaviour
    It is no longer used.

--- lib/hare/app_handler.ex
+++
@@ -1,8 +0,0 @@
-defmodule Hare.AppHandler do
-  @moduledoc """
-  Behaviour to be implemented by a module in an application using Hare
-  """
-
-  @callback message_received(Tortoise.topic_filter(), String.t()) :: :ok
-  @callback invalid_payload(Tortoise.topic_filter(), String.t()) :: :ok
-end
