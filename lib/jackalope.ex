654c1d0fcdf52a7c5f1a1fc18dd9478f28d7b967
Martin Gausby <martin.gausby@erlang-solutions.com>
2020-11-04 12:35:18 +0000

    Delegate the public API into the Jackalope module
    This should provide a nice interface for the end user.

--- lib/jackalope.ex
+++ lib/jackalope.ex
@@ -1,2 +1,7 @@
 defmodule Jackalope do
+  defdelegate publish(topic, payload, opts \\ []), to: Jackalope.Session
+
+  defdelegate subscribe(topic, opts \\ []), to: Jackalope.Session
+
+  defdelegate unsubscribe(topic, opts \\ []), to: Jackalope.Session
 end
