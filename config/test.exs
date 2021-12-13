892ee477910a5873a639398b1e0805828e5e4827
Frank Hunleth <fhunleth@troodon-software.com>
2021-12-13 14:55:35 -0500

    Remove config since not needed

--- config/test.exs
+++
@@ -1,5 +0,0 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
-import Config
-
-config :jackalope, :data_dir, "/tmp/jackalope"
