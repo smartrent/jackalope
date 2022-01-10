76cc4b99bfd8192e9865cf9754404fab05756f86
Frank Hunleth <fhunleth@troodon-software.com>
2022-01-10 10:14:35 -0500

    Check that all functions have specs

--- .credo.exs
+++ .credo.exs
@@ -1,11 +1,13 @@
-# config/.credo.exs
+# .credo.exs
 %{
   configs: [
     %{
       name: "default",
       checks: [
+        {Credo.Check.Readability.ImplTrue, tags: []},
         {Credo.Check.Readability.LargeNumbers, only_greater_than: 86400},
         {Credo.Check.Readability.ParenthesesOnZeroArityDefs, parens: true},
+        {Credo.Check.Readability.Specs, tags: []},
         {Credo.Check.Design.TagTODO, false}
       ]
     }
