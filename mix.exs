faff67d3559fe05e2bb83fed9ddf36a80cdfb132
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2023-05-16 10:04:21 -0400

    mix

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Jackalope.MixProject do
   defp deps() do
     [
       # {:tortoise311, "~> 0.11.0"},
-      {:tortoise311, git: "git@github.com:smartrent/tortoise311.git", branch: "main"},
+      {:tortoise311, git: "git@github.com:smartrent/tortoise311.git", branch: "testing_jfc"},
       {:dialyxir, "~> 1.1.0", only: [:test, :dev], runtime: false},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false}
